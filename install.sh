12e39c3f5548a452c5171934c80e989b04680df0
Stefan Wallin <stefan.w@festiz.com>
2012-12-13 22:00:33 +0100

    Clear screen on logout to enhance privacy

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ fi
 
 install -m 644 ${TMP_DIR}/vimrc ${INSTALL_DIR}/.vimrc
 install -m 644 ${TMP_DIR}/bashrc ${INSTALL_DIR}/${BASH_CONFIG}
+install -m 644 ${TMP_DIR}/bash_logout ${INSTALL_DIR}/.bash_logout
 install -m 644 ${TMP_DIR}/gitconfig ${INSTALL_DIR}/.gitconfig
 install -m 644 ${TMP_DIR}/.git-completion.sh ${INSTALL_DIR}/.git-completion.sh
 install -m 644 ${TMP_DIR}/.git-prompt.sh ${INSTALL_DIR}/.git-prompt.sh
